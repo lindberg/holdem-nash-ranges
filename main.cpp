12a9cab688b1ec9a41b2cd01e61c3d97391fed16
Adam Lindberg <36076976+lindberg@users.noreply.github.com>
2019-07-26 00:43:04 +0200

    Now uses regret_tree as a class instead.

--- main.cpp
+++ main.cpp
@@ -2,18 +2,16 @@
 #include "regret_tree.h"
 
 int main() {
-	int regret_tree[kNodes];
+	RegretTree regret_tree_;
 
-	memset(regret_tree, 0, sizeof(regret_tree));
-
-	if (!OpenRegretTree(regret_tree, "regret_tree.dat")) {
-		SaveRegretTree(regret_tree, "regret_tree.dat");
+	if (!regret_tree_.OpenRegretTree("regret_tree.dat")) {
+		regret_tree_.SaveRegretTree("regret_tree.dat");
 	}
 
 	int c1 = 13;
 	int c2 = 33;
 
-	std::cout << RegretIndex(c1, c2, false, true) << std::endl;
+	std::cout << regret_tree_.RegretIndex(c1, c2, false, true) << std::endl;
 
 	return 0;
 }
